b9fc67763269abe8f42ffc72907a8b436ed1417e
Alexander Nygård <iumentum@me.com>
2018-12-22 18:13:19 +0100

    Enda en fil...

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,11 @@
-print("Hello Python World!")
+message = "Hello Python Crash Course reader!"
+print(message)
+
+message = "Og så endrer vi denne meldingen"
+print(message)
+
+message += " og vi endrer"
+print(message)
+
+message += " og vi 'endrer'\ :-P"
+print(message)
---
+++ name.py
@@ -0,0 +1,7 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print("Hello, " + full_name.title() + "!")
+message = "\n Hei\n\tpå\n\t\tdeg "
+message = message.strip()
+print("'"+message+"'")
