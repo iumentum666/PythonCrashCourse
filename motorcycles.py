4404c3c988c88ef1e70a89c506ef12298eca236a
Nygård <NONYGALE@carlsberggroup.com>
2019-01-02 12:18:42 +0100

    Motorcycles gjort ferdig

--- motorcycles.py
+++ motorcycles.py
@@ -6,9 +6,26 @@ motorcycles = []
 motorcycles.append('honda')
 motorcycles.append('yamaha')
 motorcycles.append('suzuki')
+motorcycles.append('ducati')
 
-motorcycles.insert(0, 'ducati')
 print(motorcycles)
 
-del motorcycles[1]
+# Listen kan også bygges slik:
+# motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati')
+ 
+# POP kan hente enten siste verdi ved å ikke spesifisere noe: first_owned = motorcycles.pop()
+# Eller man kan hente hvilken verdi som helst slik:
+# first_owned = motorcycles.pop(0)
+# print("The first motorcycle I owned was a " + first_owned.title() + ".")b
+
+# Remove brukes hvis man ønsker å fjerne verdien, men ikke vet hvor i listen den verdien er. Remove fjerner bare den første gangen man finner verdien. Er den der flere ganger, må man bruke en loop for å identifisere alle.
+
+# motorcycles.remove('ducati')
+# print(motorcycles)
+
+# Her bruker vi en variabel for å ta en remove, og så kan man bruke den variabelen for å fortelle hva man removet
+
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
 print(motorcycles)
+print("\nA " + too_expensive.title() + " is too expensive for me.")
