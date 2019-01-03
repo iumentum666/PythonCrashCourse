d36e70fb3382a621e8d9831886c1670d4750e5fb
Nygård <NONYGALE@carlsberggroup.com>
2019-01-03 14:28:49 +0100

    ferdig med aliens dictionary

---
+++ alien 2.py
@@ -0,0 +1,11 @@
+alien_0 = {'color' : 'green' , 'points' : 5} # Et dictionary er omkranset av kr�llparanteser
+
+# print(alien_0['color']) # Man henter verdien fra et dictionary ved � spesifisere hva man skal hente i firkanter
+# print(alien_0['points'])
+
+print(alien_0)
+
+# Det er bare � legge til flere verdier i et dictionary
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
---
+++ alien 3.py
@@ -0,0 +1,7 @@
+alien_0 = {'color' : 'green' , 'points' : 5} # Et dictionary er omkranset av kr�llparanteser
+
+print(alien_0)
+
+# Det er enkelt å slette verdier
+del alien_0['points']
+print(alien_0)
