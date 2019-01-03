d066653f79142d44a6443feedcf9f564bae01103
Nygård <NONYGALE@carlsberggroup.com>
2019-01-03 09:49:09 +0100

    tuples ferdig

--- dimensions.py
+++ dimensions.py
@@ -1,4 +1,19 @@
-# Her l�rer vi om tuples. Lister som ikke kan endres
+# Her lærer vi om tuples. Lister som ikke kan endres
 dimensions =(200,50) # tuples defineres med parantes og ikke square brackets
-print(dimensions[0])
-print(dimensions[1])
+
+# print(dimensions[0])
+# print(dimensions[1])
+
+# dimensions[0] = 250 # Dette vil feile... Man kan ikke endre enn tuple slik
+
+# Man kan loope igjennom alle verdier i en tuple
+
+for dimensions in dimensions:
+    print(dimensions)
+
+# Man kan ikke modifisere en tuple, men man kan assigne nye verdier til variabelen som inneholder tupelen. Så for å endre må vi redefinere hele greia
+
+dimensions = (400, 100)
+print("\nModifid dimensions:")
+for dimensions in dimensions:
+    print(dimensions)
