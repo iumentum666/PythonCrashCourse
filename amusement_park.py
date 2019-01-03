d7865a812ad4b046f5fc68ec5db29e611fee570e
Nygård <NONYGALE@carlsberggroup.com>
2019-01-03 13:06:00 +0100

    ferdig med kapittel 5

---
+++ amusement_park.py
@@ -0,0 +1,22 @@
+age = input("Enter age: ")
+age = int(age)
+
+# Avansert versjon
+#if age < 4:
+#    print("Your admission cost is $0.")
+#elif age < 18:
+#    print("Your admission cost is $5.")
+#else: 
+#    print("Your admission cost is $10.")
+
+# Forenklet versjon
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+elif age < 65:
+    price = 10
+elif age >= 65:
+    price = 5
+
+print("Your admission cost is $" + str(price) + ".")
