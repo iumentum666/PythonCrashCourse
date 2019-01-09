42f829671fbd8ea16e20d2a9e9c6f689e7c25ac3
Nygård <NONYGALE@carlsberggroup.com>
2019-01-09 15:30:01 +0100

    slutt for dagen

--- parrot 3.py
+++ parrot 3.py
@@ -1,10 +1,15 @@
 # Her gjentar vi parrotkoden, men denne gangen med en while l�kke!
 
+# I denne varianten så bruker vi et såkalt flagg for å styre hvorvidt programmet skal kjøre eller ikke
+
 prompt = "\nTell me something, and I will repeat it back to you:"
 prompt += "\nEnter 'quit' to end the program. "
 
-message = ""
-while message != 'quit':
+active = True
+while active:
     message = input(prompt)
-    if message != 'quit':
+
+    if message == 'quit':
+        active = False
+    else:
         print(message)
