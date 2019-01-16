ae7edc4dd5284dba6e7487b58ed92b622ea340f1
iumentum666 <45870466+iumentum666@users.noreply.github.com>
2019-01-16 15:40:25 +0100

    fiks for path issue. Koden funker nå i windows

--- Kapittel 10/file_reader.py
+++ Kapittel 10/file_reader.py
@@ -1,3 +1,3 @@
-with open('pi_digits.txt') as file_object:
+with open('C:/Users/nonygale/source/repos/y/PythonCrashCourse/Kapittel 10/pi_digits.txt') as file_object:
     contents = file_object.read()
     print(contents)
