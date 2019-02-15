9eaf52316002f33e4cc83a9612b965b5c99c3497
iumentum666 <45870466+iumentum666@users.noreply.github.com>
2019-02-15 13:16:18 +0100

    I gang med første prosjekt

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,29 @@
+import pygame
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+    # Initialize pygame, settings and screen object.
+
+    pygame.init()
+    ai_settings = Settings()
+
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship.
+    ship = Ship(screen)
+
+    # Start the main loop for the game.
+    while True:
+
+        # Watch for keyboard and mouse events.
+        gf.check_events(ship)
+
+        # Redraw the screen during each pass through the loop.
+        gf.update_screen(ai_settings, screen, ship)
+
+run_game()
